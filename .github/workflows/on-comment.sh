5d69ed0ca7150230c83c724057e386ff313682fb
Jan-Erik Rediger <janerik@fnordig.de>
2026-01-12 12:30:46 +0100

    show statuses

--- .github/workflows/on-comment.sh
+++ .github/workflows/on-comment.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 set -eux
+
+STATUSES_URL=$(curl https://api.github.com/repos/mozilla/glean/pulls/3371 | jq -r .statuses_url)
+curl "$STATUSES_URL" > statuses.json
+cat statuses.json
+
+<statuses.json jq '.[] | select(.state=="pending")'
