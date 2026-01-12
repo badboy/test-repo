8271fddd836a5d2d5f0fe346ffcb95b0419317b6
Jan-Erik Rediger <janerik@fnordig.de>
2026-01-12 12:39:05 +0100

    Print work and job id

--- .github/workflows/on-comment.sh
+++ .github/workflows/on-comment.sh
@@ -6,4 +6,11 @@ STATUSES_URL=$(curl "https://api.github.com/repos/badboy/test-repo/pulls/${PR_NU
 curl "$STATUSES_URL" > statuses.json
 cat statuses.json
 
-<statuses.json jq '.[] | select(.state=="pending")'
+workflow_url=$(<statuses.json jq '.[] | select(.state=="pending" and .context=="ci/circleci: iOS/hold") | .target_url' -r | uniq)
+workflow_id=$(echo "$workflow_url" | cut -d'/' -f 5)
+echo "Workflow ID: $workflow_id"
+
+job_id=$(curl --request GET \
+  "https://circleci.com/api/v2/workflow/$workflow_id/job" \
+  --header "Circle-Token: $CIRCLE_TOKEN" | jq -r '.items[] | select(.name=="hold" and .status=="on_hold") | .id')
+echo "Job ID: $job_id"
