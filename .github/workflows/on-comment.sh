25ebb927defa3f6841702d82a3e59e9d20eeb4dd
Jan-Erik Rediger <janerik@fnordig.de>
2026-01-12 12:41:42 +0100

    use the correct job

--- .github/workflows/on-comment.sh
+++ .github/workflows/on-comment.sh
@@ -6,11 +6,21 @@ STATUSES_URL=$(curl "https://api.github.com/repos/badboy/test-repo/pulls/${PR_NU
 curl "$STATUSES_URL" > statuses.json
 cat statuses.json
 
-workflow_url=$(<statuses.json jq '.[] | select(.state=="pending" and .context=="ci/circleci: iOS/hold") | .target_url' -r | uniq)
+workflow_url=$(<statuses.json jq '.[] | select(.state=="pending" and .context=="ci/circleci: ci/hold") | .target_url' -r | uniq)
 workflow_id=$(echo "$workflow_url" | cut -d'/' -f 5)
 echo "Workflow ID: $workflow_id"
+
+if [[ -n "$workflow_id" ]]; then
+  echo "workflow ID is missing"
+  exit 0
+fi
 
 job_id=$(curl --request GET \
   "https://circleci.com/api/v2/workflow/$workflow_id/job" \
   --header "Circle-Token: $CIRCLE_TOKEN" | jq -r '.items[] | select(.name=="hold" and .status=="on_hold") | .id')
 echo "Job ID: $job_id"
+
+if [[ -n "$job_id" ]]; then
+  echo "job ID is missing"
+  exit 0
+fi
